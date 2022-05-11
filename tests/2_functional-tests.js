d14aa2fb7a7249b424935b23e728d1c0f3626281
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-05-11 00:49:58 -0400

    Run Functional Tests on an API Response using Chai-HTTP III - PUT method

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,10 +36,14 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({
+          surname: "Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json")
+          assert.equal(res.body.name, "Cristoforo")
+          assert.equal(res.body.surname, "Colombo")
           done();
         });
     });
