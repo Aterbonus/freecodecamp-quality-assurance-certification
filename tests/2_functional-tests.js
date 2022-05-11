6294769b837787ec3a87bc86cf37ae33a65a92f6
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-05-11 00:53:08 -0400

    Run Functional Tests on an API Response using Chai-HTTP IV - PUT method

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -49,7 +49,18 @@ suite('Functional Tests', function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({
+          surname: "da Verrazzano"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200)
+          assert.equal(res.type, "application/json")
+          assert.equal(res.body.name, "Giovanni")
+          assert.equal(res.body.surname, "da Verrazzano")
+        })
 
       done();
     });
