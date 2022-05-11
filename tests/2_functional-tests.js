a0c18267f2a4a8bab95e06951fabd9a3f996ef5d
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-05-11 00:57:23 -0400

    Simulate Actions Using a Headless Browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -68,11 +68,16 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://localhost:3000'
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
+  const browser = new Browser()
 
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
