a0e29dfbd4703bb228e4fc8e76270294af1cf58f
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-05-11 01:19:35 -0400

    Run Functional Tests Using a Headless Browser II

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -101,9 +101,15 @@ suite('Functional Tests with Zombie.js', function () {
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci')
+      .then(() => browser.pressButton('submit'))
+      .then(() => {
+        browser.assert.success()
+        browser.assert.text('span#name', 'Amerigo')
+        browser.assert.text('span#surname', 'Vespucci')
+        browser.assert.elements('span#dates', 1)
+        done();
+      })
     });
   });
 });
